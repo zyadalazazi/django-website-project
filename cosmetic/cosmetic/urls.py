257b242bc4d80e262881a4d565a2a48438ee50a0
zyadalazazi <zyadazazi@gmail.com>
2022-01-27 12:42:43 +0300

    Added models and the main page

--- cosmetic/cosmetic/urls.py
+++ cosmetic/cosmetic/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from makeup.views import welcome_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('main/', welcome_page)
 ]
--- cosmetic/makeup/models.py
+++ cosmetic/makeup/models.py
@@ -3,22 +3,28 @@ from django.db import models
 # Create your models here.
 
 
-class Brand:
+class Brand(models.Model):
     name = models.CharField(max_length=50)
     origin = models.CharField(max_length=50)
 
     def __str__(self):
-        pass
+        return self.name
 
     def get_absolute_url(self):
         pass
 
 
-class Product:
+class Product(models.Model):
     name = models.CharField(max_length=50)
     kind = models.CharField(max_length=50)
     description = models.TextField()
     expire_date = models.DateField()
     price = models.FloatField()
     brand = models.ForeignKey('Brand', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "The name is {0}, its price is {1} and its brand is {2}".format(self.name, self.price, self.brand)
+
+    def get_absolute_url(self):
+        pass
 
